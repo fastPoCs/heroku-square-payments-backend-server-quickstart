f5fce8e0c97694722bb67ab856e26ba43a519e47
imlieing <jeechieu.ta321@gmail.com>
2021-10-31 19:44:11 -0700

    testing

--- util.js
+++ util.js
@@ -3,19 +3,19 @@ var Airtable = require('airtable');
 function appendJsonToAirtable(jsonToAppend) {
     var base = new Airtable({apiKey: 'key9wXBUzD4Ipemsg'}).base('appmiRjzJTfk1VVWm');
 
-    base('purchases').create([
-        {
-          jsonToAppend
-        },
-      ], function(err, records) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        records.forEach(function (record) {
-          console.log(record.getId());
-        });
-      });
+    // base('purchases').create([
+    //     {
+    //       jsonToAppend
+    //     },
+    //   ], function(err, records) {
+    //     if (err) {
+    //       console.error(err);
+    //       return;
+    //     }
+    //     records.forEach(function (record) {
+    //       console.log(record.getId());
+    //     });
+    //   });
 
 }
 module.exports = { appendJsonToAirtable };
